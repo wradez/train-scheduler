d127510c6948e2986f90a61ea31afe78f516e131
Brook Trout <walkerradez@gmail.com>
2018-05-08 10:08:33 -0600

    Initial file setup and external links setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+var config = {
+apiKey: "AIzaSyCDckFcQIe70SVJD2be05nkdEENTsSvYEw",
+authDomain: "train-scheduler-c3b9b.firebaseapp.com",
+databaseURL: "https://train-scheduler-c3b9b.firebaseio.com",
+projectId: "train-scheduler-c3b9b",
+storageBucket: "",
+messagingSenderId: "664774837557"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
